23ea3fd849ad2616ebd9b36a98edc24fd3f32b98
Venkatraman Renganathan <vrengana@utdallas.edu>
2020-10-05 18:24:42 -0500

    Update lqr_internal_external_controller.py

--- Summer 2020 Code/Multirotor API Testing/PWM model/lqr_internal_external_controller.py
+++ Summer 2020 Code/Multirotor API Testing/PWM model/lqr_internal_external_controller.py
@@ -28,42 +28,6 @@ from scipy.spatial.transform import Rotation as scipy_rotation
 RADS2RPM = 60/(2*np.pi)
 RAD2DEG  = 180/np.pi
 
-##############################################################################
-##############################################################################
-###########################  DEFINE THE FUNCTIONS  ###########################
-##############################################################################
-##############################################################################
-
-def not_reached(pt1, pt2, dist):
-    if np.linalg.norm(pt1[0:3] - pt2[0:3]) > dist:
-        return True
-    else:
-        return False
-    
-##############################################################################
-##############################################################################
-
-def GetRotationMatrix(axis_flag, angle):
-    if axis_flag == 1: # x axis
-        rotationMatrix = np.array([[1, 0, 0],
-                                   [0, np.cos(angle), -np.sin(angle)],
-                                   [0, np.sin(angle), np.cos(angle)]])
-        
-    if axis_flag == 2: # y axis
-        rotationMatrix = np.array([[np.cos(angle), 0, np.sin(angle)],
-                                   [0, 1, 0],
-                                   [-np.sin(angle), 0, np.cos(angle)]])
-        
-    if axis_flag == 3: # z axis
-        rotationMatrix = np.array([[np.cos(angle), -np.sin(angle), 0],
-                                   [np.sin(angle), np.cos(angle), 0],
-                                   [0, 0, 1]])
-    
-    return rotationMatrix
-
-    
-##############################################################################
-##############################################################################    
 
 # System parameters
 pwmHover            = 0.59375                                                # PWM hover constant
@@ -130,8 +94,45 @@ k                   = Tmax/(max_omega**2)# Traction coefficient
 b                   = Qmax/(max_omega**2)# Moment coefficient
 c                   = (0.04-0.0035) # Lumped Drag constant
 KB                  = 2
-sq_ctrl_hover      = pwmHover*(max_omega**2)*((2*np.pi)**2) 
+sq_ctrl_hover       = pwmHover*(max_omega**2)*((2*np.pi)**2) 
 
+
+##############################################################################
+##############################################################################
+###########################  DEFINE THE FUNCTIONS  ###########################
+##############################################################################
+##############################################################################
+
+def not_reached(pt1, pt2, dist):
+    if np.linalg.norm(pt1[0:3] - pt2[0:3]) > dist:
+        return True
+    else:
+        return False
+    
+##############################################################################
+##############################################################################
+
+def GetRotationMatrix(axis_flag, angle):
+    if axis_flag == 1: # x axis
+        rotationMatrix = np.array([[1, 0, 0],
+                                   [0, np.cos(angle), -np.sin(angle)],
+                                   [0, np.sin(angle), np.cos(angle)]])
+        
+    if axis_flag == 2: # y axis
+        rotationMatrix = np.array([[np.cos(angle), 0, np.sin(angle)],
+                                   [0, 1, 0],
+                                   [-np.sin(angle), 0, np.cos(angle)]])
+        
+    if axis_flag == 3: # z axis
+        rotationMatrix = np.array([[np.cos(angle), -np.sin(angle), 0],
+                                   [np.sin(angle), np.cos(angle), 0],
+                                   [0, 0, 1]])
+    
+    return rotationMatrix
+
+    
+##############################################################################
+##############################################################################
 
 def External_Loop(Ts = 0.1, max_angular_vel = 6396.667 * 2* np.pi/ 60):
 
@@ -226,6 +227,109 @@ def Internal_Loop(Ts = 0.1, max_angular_vel = 6396.667 * 2* np.pi/ 60):
     _, K_int = matrixmath.dare_gain(sysd[0],sysd[1], Q, R)
 
     return K_int, u0, Gamma, CT
+
+
+##############################################################################
+##############################################################################    
+
+def Get_Linearized_Dynamics(state_k, Ts = 0.1, max_angular_vel = 6396.667 * 2* np.pi/ 60):
+    
+    x     = state_k[0,0]
+    y     = state_k[1,0]
+    z     = state_k[2,0]
+    phi   = state_k[3,0]
+    theta = state_k[4,0]
+    psi   = state_k[5,0]
+    u     = state_k[6,0]
+    v     = state_k[7,0]
+    w     = state_k[8,0]
+    p     = state_k[9,0]
+    q     = state_k[10,0]
+    r     = state_k[11,0]
+    
+    Iy_z = (Iyy - Izz)/Ixx
+    Iz_x = (Izz - Ixx)/Iyy
+    Ix_y = (Ixx - Iyy)/Izz
+    
+    num_states   = 12
+    num_controls = 4
+    
+    A = np.zeros((num_states, num_states))
+    
+    A[0,3]   = v*theta - w*psi 
+    A[0,4]   = v*phi + w
+    A[0,5]   = -v + w*phi
+    A[0,6]   = 1
+    A[0,7]   = phi*theta - psi
+    A[0,8]   = phi*psi + theta
+    A[1,3]   = v*psi*theta - w
+    A[1,4]   = v*phi*psi + w*psi
+    A[1,5]   = u + v*phi*theta + w*theta
+    A[1,6]   = psi
+    A[1,7]   = phi*psi*theta + 1
+    A[1,8]   = psi*theta - phi
+    A[2,3]   = v
+    A[2,4]   = -u
+    A[2,6]   = -theta
+    A[2,7]   = phi
+    A[2,8]   = 1
+    A[3,3]   = q*theta 
+    A[3,4]   = q*phi + r
+    A[3,9]   = 1
+    A[3,10]  = phi*theta
+    A[3,11]  = theta
+    A[4,3]   = -r
+    A[4,10]  = 1
+    A[4,11]  = -phi
+    A[5,3]   = q
+    A[5,10]  = phi
+    A[5,11]  = 1
+    A[6,4]   = -g
+    A[6,7]   = r
+    A[6,8]   = -q
+    A[6,10]  = -w
+    A[6,11]  = v
+    A[7,3]   = g
+    A[7,6]   = -r
+    A[7,8]   = p
+    A[7,9]   = w
+    A[7,11]  = -u
+    A[8,6]   = q
+    A[8,7]   = -p
+    A[8,9]   = -v
+    A[8,10]  = u    
+    A[9,10]  = Iy_z*r
+    A[9,11]  = Iy_z*q   
+    A[10,9]  = Iz_x*r
+    A[10,11] = Iz_x*p
+    A[11,9]  = Ix_y*q
+    A[11,10] = Ix_y*p 
+    
+    # Control Input matrix
+    B = np.array([[0,   0,     0,       0],
+                  [0,   0,     0,       0],
+                  [0,   0,     0,       0],
+                  [0,   0,     0,       0],
+                  [0,   0,     0,       0],
+                  [0,   0,     0,       0],
+                  [0,   0,     0,       0],
+                  [0,   0,     0,       0],
+                  [1/m, 0,     0,       0],                  
+                  [0,   1/Ixx, 0,       0],
+                  [0,   0,     1/Iyy,   0],
+                  [0,   0,     0,       1/Izz]])
+    
+    C = np.identity(num_states)
+    D = np.zeros((num_states, num_controls))
+    
+    # Form the contnuous state space model
+    sys  = signal.lti(A,B,C,D)
+    
+    # Discretize it using the time step Ts 
+    sysd = signal.cont2discrete((sys.A,sys.B,sys.C,sys.D),Ts)    
+
+    return sysd[0], sysd[1]    
+    
     
 ##############################################################################
 ##############################################################################
@@ -249,6 +353,11 @@ def main():
     g               = 9.81                      # Gravity  
     mass            = 1.0                       # Mass
     pwmHover        = 0.59375 
+    
+    # Define the state and control penalty matrices    
+    Q = np.diag([1,1,1,1,1,1,1,1,0.6,60,60,1e-5]) # np.identity(12)
+    R = 0.01*np.identity(4)
+    
     # Specify the goal state to be reached
     x_goal = np.array([[1.0],
                        [0.0],
@@ -286,7 +395,7 @@ def main():
         multirotorClient.takeoffAsync().join()
     else:
         multirotorClient.hoverAsync().join()            
-        time.sleep(1)
+        time.sleep(5)
     
     # Infer the drone position
     pos = state.kinematics_estimated.position
@@ -368,21 +477,29 @@ def main():
         
         # Define the error to goal
         error = x - x_goal        
-        # print('error=', error)        
         
-        e_ext = error[[0,1,6,7],:]        
-        u_ext = np.dot(K_ext, e_ext)
-        # print(u_ext)
-        e_int = error[[2, 3, 4,5,8,9,10,11],:]   
-        e_int[[1,0]] += u_ext[[0,0]]
-        e_int[[2,0]] += u_ext[[1,0]]
-        u_int = np.dot(K_int, e_int)  + u0
+        # Get the dynamics linearized around the current state
+        A,B = Get_Linearized_Dynamics(x)
+        
+        _, K_full = matrixmath.dare_gain(A, B, Q, R)
+        
+        u_full = K_full @ error + u0        
+        
+        
+        # e_ext = error[[0,1,6,7],:]        
+        # u_ext = np.dot(K_ext, e_ext)
+        # # print(u_ext)
+        # e_int = error[[2, 3, 4,5,8,9,10,11],:]   
+        # e_int[[1,0]] += u_ext[[0,0]]
+        # e_int[[2,0]] += u_ext[[1,0]]
+        # u_int = np.dot(K_int, e_int) + u0
         
         
         ##############################################################################
         ###########################  PWM CONTROL  ####################################
         # Convext [f,tx,ty,tz] to pwm
-        pwm  = np.clip(( Gamma.T @ u_int)/(max_omega**2),0,1)
+        # pwm  = np.clip((Gamma @ u_int)/(max_omega**2),0,1)
+        pwm  = np.clip((Gamma @ u_full)/(max_omega**2),0,1)
         
         # Apply the PWM to Airsim
         multirotorClient.moveByMotorPWMsAsync(pwm[0,0], pwm[1,0], pwm[2,0], pwm[3,0],Ts).join
